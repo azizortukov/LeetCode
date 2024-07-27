df322763acabd591fd574ba6a3223af11ebc741b
azizortukov <azizortukov818@gmail.com>
2024-07-27 18:03:51 +0500

    BV by 42ms: 1. Two Sum

--- src/arrays/Solutions.java
+++ src/arrays/Solutions.java
@@ -1,8 +1,6 @@
 package arrays;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Solutions {
 
@@ -179,15 +177,16 @@ public class Solutions {
      */
 
     public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
         int[] result = new int[2];
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (target == nums[i] + nums[j]) {
-                    result[0] = i;
-                    result[1] = j;
-                    return result;
-                }
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[1] = i;
+                result[0] = map.get(target - nums[i]);
+                return result;
             }
+            map.put(nums[i], i);
         }
         return result;
     }
