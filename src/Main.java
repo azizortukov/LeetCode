4d1ab6f11c400d03514f4021ca2f9d1779b623d8
azizortukov <azizortukov818@gmail.com>
2024-07-27 18:02:52 +0500

    1. Two Sum

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,9 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
         Solutions solution = new Solutions();
-        System.out.println(solution.kidsWithCandies(
-                new int[]{2, 3, 5, 1, 3}, 3
-        ));
+        System.out.println(Arrays.toString(solution.twoSum(
+                new int[]{2, 7, 11, 15}, 9
+        )));
     }
 
 }
--- src/arrays/Solutions.java
+++ src/arrays/Solutions.java
@@ -173,4 +173,23 @@ public class Solutions {
         return list;
     }
 
+    /*
+    1. Two Sum
+    For more info: https://leetcode.com/problems/two-sum/submissions
+     */
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target == nums[i] + nums[j]) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+
 }
