c395a5fbb32ef0bf1446424b7aac9669de6a45d8
azizortukov <azizortukov818@gmail.com>
2024-07-29 16:40:00 +0500

    2824. Count Pairs Whose Sum is Less than Target

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@ import arrays.Solutions;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Solutions solution = new Solutions();
-        System.out.println(solution.maxWidthOfVerticalArea(
-                new int[][]{{8, 7}, {9, 9}, {7, 4}, {9, 7}}
-        ));
+        System.out.println(solution.countPairs(List.of(-1, 1, 2, 3, 1), 2));
     }
 
 }
--- src/arrays/Solutions.java
+++ src/arrays/Solutions.java
@@ -228,4 +228,21 @@ public class Solutions {
         return maxWidth;
     }
 
+    /*
+    2824. Count Pairs Whose Sum is Less than Target
+    For more info: https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/description
+     */
+
+    public int countPairs(List<Integer> nums, int target) {
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums.get(i) + nums.get(j) < target) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
 }
